8ac4120f4b381024049053b06a3fa7d81a7bf7b3
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-07-10 10:22:39 +0100

    jnjnjn

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,10 @@ export const metadata = {
     ? new URL(`https://${process.env.VERCEL_URL}`)
     : undefined,
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Forex Bot AI',
+    template: `%s - Forex AI Chatbot`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'An AI-powered Forex Bot  built with Phython, Claude Opus and Open Ai.',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,114 @@
-import * as React from 'react'
-import Link from 'next/link'
+// import * as React from 'react'
+// import Link from 'next/link'
 
-import { cn } from '@/lib/utils'
-import { auth } from '@/auth'
-import { Button, buttonVariants } from '@/components/ui/button'
+// import { cn } from '@/lib/utils'
+// import { auth } from '@/auth'
+// import { Button, buttonVariants } from '@/components/ui/button'
+// import {
+//   IconGitHub,
+//   IconNextChat,
+//   IconSeparator,
+//   IconVercel
+// } from '@/components/ui/icons'
+// import { UserMenu } from '@/components/user-menu'
+// import { SidebarMobile } from './sidebar-mobile'
+// import { SidebarToggle } from './sidebar-toggle'
+// import { ChatHistory } from './chat-history'
+// import { Session } from '@/lib/types'
+
+// async function UserOrLogin() {
+//   const session = (await auth()) as Session
+//   return (
+//     <>
+//       {session?.user ? (
+//         <>
+//           <SidebarMobile>
+//             <ChatHistory userId={session.user.id} />
+//           </SidebarMobile>
+//           <SidebarToggle />
+//         </>
+//       ) : (
+//         <Link href="/dashboard" rel="nofollow">
+//           <img alt='forex bot' className='size-6 rounded-full mr-2' src="https://uoppdgbycojkpcadszya.supabase.co/storage/v1/object/public/documents/forex_bot.jpg"/>
+//           <img alt='forex bot' className='size-6 rounded-full mr-2 dark:block' src="https://uoppdgbycojkpcadszya.supabase.co/storage/v1/object/public/documents/forex_bot.jpg" />
+//         </Link>
+//       )}
+//       <div className="flex items-center">
+//         <IconSeparator className="size-6 text-muted-foreground/50" />
+//  FOREX BOT
+//       </div>
+//     </>
+//   )
+// }
+
+// export async function Header() {
+//   const session = (await auth()) as Session
+//   return (
+//     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+//       <div className="flex items-center">
+//         <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
+//           <UserOrLogin />
+//         </React.Suspense>
+//       </div>
+//       <div className="flex items-center justify-end space-x-2">
+//       {session?.user ? (
+//           <UserMenu user={session.user} />
+//         ) : (
+//           <Button variant="link" asChild className="-ml-2">
+//             <Link href="/login">Login</Link>
+//           </Button>
+//         )}
+// <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+//   <span className="absolute inset-0 overflow-hidden rounded-full">
+//     <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+//   </span>
+//   <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+//     <span>
+//       Get started
+//     </span>
+//     <svg
+//       fill="none"
+//       height="16"
+//       viewBox="0 0 24 24"
+//       width="16"
+//       xmlns="http://www.w3.org/2000/svg"
+//     >
+//       <path
+//         d="M10.75 8.75L14.25 12L10.75 15.25"
+//         stroke="currentColor"
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//         strokeWidth="1.5"
+//       />
+//     </svg>
+//   </div>
+//   <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+// </button>
+//       </div>
+//     </header>
+//   )
+// }
+
+
+import * as React from 'react';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { auth } from '@/auth';
+import { Button, buttonVariants } from '@/components/ui/button';
 import {
   IconGitHub,
   IconNextChat,
   IconSeparator,
-  IconVercel
-} from '@/components/ui/icons'
-import { UserMenu } from '@/components/user-menu'
-import { SidebarMobile } from './sidebar-mobile'
-import { SidebarToggle } from './sidebar-toggle'
-import { ChatHistory } from './chat-history'
-import { Session } from '@/lib/types'
+  IconVercel,
+} from '@/components/ui/icons';
+import { UserMenu } from '@/components/user-menu';
+import { SidebarMobile } from './sidebar-mobile';
+import { SidebarToggle } from './sidebar-toggle';
+import { ChatHistory } from './chat-history';
+import { Session } from '@/lib/types';
 
 async function UserOrLogin() {
-  const session = (await auth()) as Session
+  const session = (await auth()) as Session;
   return (
     <>
       {session?.user ? (
@@ -29,20 +120,28 @@ async function UserOrLogin() {
         </>
       ) : (
         <Link href="/dashboard" rel="nofollow">
-          <img alt='forex bot' className='size-6 rounded-full mr-2' src="https://uoppdgbycojkpcadszya.supabase.co/storage/v1/object/public/documents/forex_bot.jpg"/>
-          <img alt='forex bot' className='size-6 rounded-full mr-2 dark:block' src="https://uoppdgbycojkpcadszya.supabase.co/storage/v1/object/public/documents/forex_bot.jpg" />
+          <img
+            alt="forex bot"
+            className="size-6 rounded-full mr-2"
+            src="https://uoppdgbycojkpcadszya.supabase.co/storage/v1/object/public/documents/forex_bot.jpg"
+          />
+          <img
+            alt="forex bot"
+            className="size-6 rounded-full mr-2 dark:block"
+            src="https://uoppdgbycojkpcadszya.supabase.co/storage/v1/object/public/documents/forex_bot.jpg"
+          />
         </Link>
       )}
       <div className="flex items-center">
         <IconSeparator className="size-6 text-muted-foreground/50" />
- FOREX BOT
+        FOREX BOT
       </div>
     </>
-  )
+  );
 }
 
 export async function Header() {
-  const session = (await auth()) as Session
+  const session = (await auth()) as Session;
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
@@ -51,40 +150,38 @@ export async function Header() {
         </React.Suspense>
       </div>
       <div className="flex items-center justify-end space-x-2">
-      {session?.user ? (
+        {session?.user ? (
           <UserMenu user={session.user} />
         ) : (
           <Button variant="link" asChild className="-ml-2">
             <Link href="/login">Login</Link>
           </Button>
         )}
-<button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-  <span className="absolute inset-0 overflow-hidden rounded-full">
-    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-  </span>
-  <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-    <span>
-      Get started
-    </span>
-    <svg
-      fill="none"
-      height="16"
-      viewBox="0 0 24 24"
-      width="16"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M10.75 8.75L14.25 12L10.75 15.25"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="1.5"
-      />
-    </svg>
-  </div>
-  <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-</button>
+        <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
+          <span className="absolute inset-0 overflow-hidden rounded-full">
+            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+          </span>
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+            <span>Get started</span>
+            <svg
+              fill="none"
+              height="16"
+              viewBox="0 0 24 24"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.75 8.75L14.25 12L10.75 15.25"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+              />
+            </svg>
+          </div>
+          <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+        </button>
       </div>
     </header>
-  )
+  );
 }
