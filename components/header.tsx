f99cc999ce809b099a40d54fe63db17a7c4b055e
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-07-13 16:45:50 +0100

    vfgrg

--- components/header.tsx
+++ components/header.tsx
@@ -1,128 +1,3 @@
-// "use client"
-// import * as React from 'react'
-// import Link from 'next/link'
-
-// import { cn } from '@/lib/utils'
-// import { auth } from '@/auth'
-// import { Button, buttonVariants } from '@/components/ui/button'
-// import {
-//   IconGitHub,
-//   IconNextChat,
-//   IconSeparator,
-//   IconVercel
-// } from '@/components/ui/icons'
-// import { UserMenu } from '@/components/user-menu'
-// import { SidebarMobile } from './sidebar-mobile'
-// import { SidebarToggle } from './sidebar-toggle'
-// import { ChatHistory } from './chat-history'
-// import { Session } from '@/lib/types'
-// import { usePathname } from 'next/navigation';
-
-// async function UserOrLogin() {
-//   const session = (await auth()) as Session
-//   const pathname = usePathname();
-//   const isRootPage = pathname === '/';
-//   return (
-//     <>
-//     {isRootPage && session?.user ? (
-//       <>
-//         <SidebarMobile>
-//           <ChatHistory userId={session.user.id} />
-//         </SidebarMobile>
-//         <SidebarToggle />
-//       </>
-//     ) : (
-//       <Link href="/dashboard">
-//         <img
-//           alt="forex bot"
-//           className="size-8 rounded-full mr-2"
-//           src="https://uoppdgbycojkpcadszya.supabase.co/storage/v1/object/public/documents/forex_bot.jpg"
-//         />
-//       </Link>
-//     )}
-//     <div className="flex items-center">
-//       <IconSeparator className="size-6 text-muted-foreground/50" />
-//       <p className="font-bold text-white font-serif text-pretty">FOREX BOT</p>
-//     </div>
-//   </>
-//   )
-// }
-
-// export async function Header() {
-//   const session = (await auth()) as Session;
-//   return (
-//     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-//       <div className="flex items-center">
-//         <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
-//           <UserOrLogin />
-//         </React.Suspense>
-//       </div>
-//       <div className="flex items-center justify-end space-x-4">
-//       <div className="border border-blue-700 align-middle items-center shadow-xl rounded-md">
-//         <Link href="/dashboard">
-//         <svg
-//     viewBox="0 0 24 24"
-//     version="1.1"
-//     xmlns="http://www.w3.org/2000/svg"
-//     xmlnsXlink="http://www.w3.org/1999/xlink"
-//     fill="#101fea"
-//     stroke="#101fea"
-//     className="size-5 aligm-middle"
-//   >
-//     <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-//     <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-//     <g id="SVGRepo_iconCarrier">
-//       <title>ic_fluent_bot_24_regular</title>
-//       <desc>Created with Sketch.</desc>
-//       <g id="🔍-Product-Icons" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-//         <g id="ic_fluent_bot_24_regular" fill="#0b24da" fillRule="nonzero">
-//           <path d="M17.7530511,13.999921 C18.9956918,13.999921 20.0030511,15.0072804 20.0030511,16.249921 L20.0030511,17.1550008 C20.0030511,18.2486786 19.5255957,19.2878579 18.6957793,20.0002733 C17.1303315,21.344244 14.8899962,22.0010712 12,22.0010712 C9.11050247,22.0010712 6.87168436,21.3444691 5.30881727,20.0007885 C4.48019625,19.2883988 4.00354153,18.2500002 4.00354153,17.1572408 L4.00354153,16.249921 C4.00354153,15.0072804 5.01090084,13.999921 6.25354153,13.999921 L17.7530511,13.999921 Z M17.7530511,15.499921 L6.25354153,15.499921 C5.83932796,15.499921 5.50354153,15.8357075 5.50354153,16.249921 L5.50354153,17.1572408 C5.50354153,17.8128951 5.78953221,18.4359296 6.28670709,18.8633654 C7.5447918,19.9450082 9.44080155,20.5010712 12,20.5010712 C14.5599799,20.5010712 16.4578003,19.9446634 17.7186879,18.8621641 C18.2165778,18.4347149 18.5030511,17.8112072 18.5030511,17.1550005 L18.5030511,16.249921 C18.5030511,15.8357075 18.1672647,15.499921 17.7530511,15.499921 Z M11.8985607,2.00734093 L12.0003312,2.00049432 C12.380027,2.00049432 12.6938222,2.2826482 12.7434846,2.64872376 L12.7503312,2.75049432 L12.7495415,3.49949432 L16.25,3.5 C17.4926407,3.5 18.5,4.50735931 18.5,5.75 L18.5,10.254591 C18.5,11.4972317 17.4926407,12.504591 16.25,12.504591 L7.75,12.504591 C6.50735931,12.504591 5.5,11.4972317 5.5,10.254591 L5.5,5.75 C5.5,4.50735931 6.50735931,3.5 7.75,3.5 L11.2495415,3.49949432 L11.2503312,2.75049432 C11.2503312,2.37079855 11.5324851,2.05700336 11.8985607,2.00734093 L12.0003312,2.00049432 L11.8985607,2.00734093 Z M16.25,5 L7.75,5 C7.33578644,5 7,5.33578644 7,5.75 L7,10.254591 C7,10.6688046 7.33578644,11.004591 7.75,11.004591 L16.25,11.004591 C16.6642136,11.004591 17,10.6688046 17,10.254591 L17,5.75 C17,5.33578644 16.6642136,5 16.25,5 Z M9.74928905,6.5 C10.4392523,6.5 10.9985781,7.05932576 10.9985781,7.74928905 C10.9985781,8.43925235 10.4392523,8.99857811 9.74928905,8.99857811 C9.05932576,8.99857811 8.5,8.43925235 8.5,7.74928905 C8.5,7.05932576 9.05932576,6.5 9.74928905,6.5 Z M14.2420255,6.5 C14.9319888,6.5 15.4913145,7.05932576 15.4913145,7.74928905 C15.4913145,8.43925235 14.9319888,8.99857811 14.2420255,8.99857811 C13.5520622,8.99857811 12.9927364,8.43925235 12.9927364,7.74928905 C12.9927364,7.05932576 13.5520622,6.5 14.2420255,6.5 Z"></path>
-//         </g>
-//       </g>
-//     </g>
-//   </svg>
-//         </Link>
-   
-// </div>
-
-//         {session?.user ? (
-//           <UserMenu user={session.user} />
-//         ) : (
-//           <Link href="/signup">
-//              <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
-//           <span className="absolute inset-0 overflow-hidden rounded-full">
-//             <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-//           </span>
-//           <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-//             <span>Get started</span>
-//             <svg
-//               fill="none"
-//               height="16"
-//               viewBox="0 0 24 24"
-//               width="16"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path
-//                 d="M10.75 8.75L14.25 12L10.75 15.25"
-//                 stroke="currentColor"
-//                 strokeLinecap="round"
-//                 strokeLinejoin="round"
-//                 strokeWidth="1.5"
-//               />
-//             </svg>
-//           </div>
-//           <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-//         </button>
-//           </Link>
-       
-//         )}
-      
-//       </div>
-//     </header>
-//   );
-// }
-
-
 
 import * as React from 'react'
 import Link from 'next/link'
@@ -141,13 +16,31 @@ import { SidebarMobile } from './sidebar-mobile'
 import { SidebarToggle } from './sidebar-toggle'
 import { ChatHistory } from './chat-history'
 import { Session } from '@/lib/types'
-import UserSessionInfo from './UserSessionInfo'
 
 async function UserOrLogin() {
   const session = (await auth()) as Session
   return (
     <>
-<UserSessionInfo />
+    {session?.user ? (
+      <>
+        <SidebarMobile>
+          <ChatHistory userId={session.user.id} />
+        </SidebarMobile>
+        <SidebarToggle />
+      </>
+    ) : (
+      <Link href="/dashboard">
+        <img
+          alt="forex bot"
+          className="size-8 rounded-full mr-2"
+          src="https://uoppdgbycojkpcadszya.supabase.co/storage/v1/object/public/documents/forex_bot.jpg"
+        />
+      </Link>
+    )}
+    <div className="flex items-center">
+      <IconSeparator className="size-6 text-muted-foreground/50" />
+      <p className="font-bold text-white font-serif text-pretty">FOREX BOT</p>
+    </div>
   </>
   )
 }
