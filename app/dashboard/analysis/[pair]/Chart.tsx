6852a44acca2db3cd207c1ea18d5951aec7c80a9
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-07-18 09:06:12 +0100

    kjhujbn

--- app/dashboard/analysis/[pair]/Chart.tsx
+++ app/dashboard/analysis/[pair]/Chart.tsx
@@ -1,55 +1,3 @@
-// "use client";
-// import React, { useEffect, useRef, memo } from 'react';
-
-// interface TradingViewWidgetProps {
-//   symbol: string;
-// }
-
-// const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ symbol }) => {
-//   const container = useRef<HTMLDivElement>(null);
-
-//   useEffect(() => {
-//     const script = document.createElement("script");
-//     script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-//     script.type = "text/javascript";
-//     script.async = true;
-//     script.innerHTML = `
-//       {
-//         "autosize": true,
-//         "height": "700",
-//         "symbol": "${symbol}",
-//         "interval": "D",
-//         "timezone": "Etc/UTC",
-//         "theme": "dark",
-//         "style": "1",
-//         "locale": "en",
-//         "withdateranges": true,
-//         "hide_side_toolbar": false,
-//         "allow_symbol_change": true,
-//         "details": true,
-//         "hotlist": true,
-//         "calendar": false,
-//         "studies": [
-//           "STD;MACD",
-//           "STD;EMA",
-//           "STD;Multi-Time%Period%Charts",
-//           "STD;On_Balance_Volume"
-//         ],
-//         "support_host": "https://www.tradingview.com"
-//       }`;
-//     container.current?.appendChild(script);
-//   }, [symbol]);
-
-//   return (
-//     <div className="tradingview-widget-container" ref={container} style={{ height: "400%", width: "100%" }}>
-//       <div className="tradingview-widget-container__widget" style={{ height: "calc(400% - 32px)", width: "100%" }}></div>
-//     </div>
-//   );
-// }
-
-// export default memo(TradingViewWidget);
-
-
 "use client";
 import React, { useEffect, useRef, useState } from 'react';
 import html2canvas from 'html2canvas';
@@ -95,7 +43,7 @@ const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ symbol }) => {
   }, [symbol]);
 
   const captureChart = async () => {
-    const chartElement = container.current?.querySelector('.tradingview-widget-container__widget');
+    const chartElement = container.current?.querySelector('.tradingview-widget-container__widget') as HTMLElement;
     if (!chartElement) return;
 
     const canvas = await html2canvas(chartElement);
